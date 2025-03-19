997f9b3910336ac0fbaedc46a8b3e64e93e2a04b
Pratham Rawat <prathamrawat2004@gmail.com>
2025-03-18 20:00:41 -0700

    backend-day-2

--- Frontend/job-portal-client/src/Pages/CreateJob.jsx
+++ Frontend/job-portal-client/src/Pages/CreateJob.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import Creatable from "react-select/creatable";
 
 const CreateJob = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => {
+    data.skills = selectedOption;
     console.log(data);
   };
+
+  const options = [
+    { value: "JavaScript", label: "JavaScript" },
+    { value: "C++", label: "C++" },
+    { value: "HTML", label: "HTML" },
+    { value: "CSS", label: "CSS" },
+    { value: "React", label: "React" },
+    { value: "Node", label: "Node" },
+    { value: "MongoDb", label: "MongoDb" },
+    { value: "Redux", label: "Redux" },
+  ];
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
       {/* form */}
@@ -84,7 +98,47 @@ const CreateJob = () => {
             </div>
           </div>
 
-          <input type="submit" />
+          {/* 4th row */}
+          <div className="flex flex-col lg:flex-row items-center justify-between gap-8">
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">Job Posting Date</label>
+              <input
+                type="date"
+                placeholder="Ex: 2023-11-03"
+                {...register("postingDate")}
+                className="block w-full flex-1 border-1 bg-white py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">Experience Level</label>
+              <select
+                {...register("experienceLevel")}
+                className="block w-full flex-1 border-1 bg-white py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6"
+              >
+                <option value="">Choose your experience</option>
+                <option value="NoExperience">NoExperience</option>
+                <option value="Internship">Internship</option>
+                <option value="Work remotely">remotely</option>
+              </select>
+            </div>
+          </div>
+
+          {/* 5th row */}
+          <div>
+            <label className="block mv-2 text-lg">Required Skill Sets:</label>
+            <Creatable
+              defaultValue={selectedOption}
+              onChange={setSelectedOption}
+              options={options}
+              isMulti
+              className="block w-full flex-1 border-1 bg-white py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6"
+            />
+          </div>
+
+          <input
+            type="submit"
+            className="block mt-12 bg-blue text-white font-semibold px-8 rounded cursor-pointer py-4"
+          />
         </form>
       </div>
     </div>
